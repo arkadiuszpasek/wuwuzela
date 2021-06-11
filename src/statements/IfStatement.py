258ace759d97effd84451bce7ff3a75dfda8354c
Arkadiusz Pasek <thevik444@gmail.com>
2021-06-11 11:46:34 +0200

    Also in if statements

--- src/statements/IfStatement.py
+++ src/statements/IfStatement.py
@@ -2,15 +2,16 @@ from grammar.Wuwuzela_GrammarParser import Wuwuzela_GrammarParser
 from src.expressions.LogicalExpression import LogicalExpression
 
 
-class IfStatement():
+class IfStatement:
     def __init__(self, variables, ctx: Wuwuzela_GrammarParser.IfStatementContext):
         global i
         from .Declaration import Declaration
+
         logical_expr = ctx.logicalExpression()
 
         if LogicalExpression(variables, logical_expr).value:
             for declaration in ctx.declaration():
-                Declaration(self, declaration)
+                Declaration(variables, declaration)
 
             if ctx.RBRACE():
                 return
