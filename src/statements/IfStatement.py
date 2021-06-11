9659b0c6721830814496ca64a38f0a64995977ed
SikoraKam <sikorakam3@gmail.com>
2021-06-11 11:58:40 +0200

    update wuw file

--- src/statements/IfStatement.py
+++ src/statements/IfStatement.py
@@ -4,7 +4,6 @@ from src.expressions.LogicalExpression import LogicalExpression
 
 class IfStatement:
     def __init__(self, variables, ctx: Wuwuzela_GrammarParser.IfStatementContext):
-        global i
         from .Declaration import Declaration
 
         logical_expr = ctx.logicalExpression()
@@ -13,5 +12,3 @@ class IfStatement:
             for declaration in ctx.declaration():
                 Declaration(variables, declaration)
 
-            if ctx.RBRACE():
-                return
